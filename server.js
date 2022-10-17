2d1eb1c46148f001e9102b2f526b9673034d9fa6
Isaac Falcon <ice6490@gmail.com>
2022-10-17 14:30:17 -0500

    added port variable

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
 
 const setNotes = require('./db/db.json');
+
+
+
+function createNote() {
+  
+};
+
+
+function deleteNote() {
+
+};
