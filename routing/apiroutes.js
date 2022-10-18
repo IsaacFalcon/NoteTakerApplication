d3186e898525d31a1902645ffbb93252b0369b97
Isaac Falcon <ice6490@gmail.com>
2022-10-18 17:00:41 -0500

    added new routing js files for saving data into json and adding to note taker app

---
+++ routing/apiroutes.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+let setNotes = require('../db/db.json');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    setNotes = JSON.parse(fs.readFileSync('./db/db.json'))
+    res.json(setNotes);
+});
+
+
+router.post('/notes', (req, res) => {
+    let noteModel = {
+        title:req.body.title,
+        text:req.body.text,
+        id:Math.floor(Math.random()*100)
+    }
+    setNotes.push(noteModel);
+    fs.writeFileSync('./db/db.json', JSON.stringify(setNotes))
+    res.json(setNotes)
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const PORT = process.env.PORT || 3001;
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
+// const api = require('./routes/index.js');
+const viewroutes = require('./routing/viewroutes');
+const apiroutes = require('./routing/apiroutes');
+const PORT = process.env.port || 3001;
+
 const app = express();
 
-const setNotes = require('./db/db.json');
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiroutes);
+app.use('/', viewroutes);
+app.use(express.static('public'));
 
 
-
-function createNote() {
-  
-};
-
-
-function deleteNote() {
-
-};
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
