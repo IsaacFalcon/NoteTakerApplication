c8ca3bcea51462993c1b64297d0131fb9702a5bd
Isaac Falcon <ice6490@gmail.com>
2022-10-19 15:21:04 -0500

    small line change

--- routing/viewroutes.js
+++ routing/viewroutes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
-// GET Route for feedback page
+
 router.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
